2c0656ef90c3940218a65fb85b0f2ba4bf81e391
Ron Friedman <ron852719@icloud.com>
2022-12-22 21:57:52 -0800

    Initial Commit

---
+++ BearMinimum/BearMinimumApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearMinimumApp.swift
+//  BearMinimum
+//
+//  Created by Ron Friedman on 2022-12-22.
+//
+
+import SwiftUI
+
+@main
+struct BearMinimumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BearMinimum/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BearMinimum
+//
+//  Created by Ron Friedman on 2022-12-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
