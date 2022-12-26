ab69ecdd83767459703e0f0bf03c78fb809b795a
Ron Friedman <ron852719@icloud.com>
2022-12-26 00:30:24 -0800

    - Simplified SwiftUI code

--- BearMinimum/ContentView.swift
+++ BearMinimum/ContentView.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
     @State var tasks = TaskManager.tasks
+    @AppStorage("CurrentStreak") var dayCount = 0
+    @State var successPlayer: AVAudioPlayer!
+    @State var yipeePlayer: AVAudioPlayer!
+    
     var body: some View {
-        let yellow = tasks.filter(\.isComplete).count == tasks.count
+        let completeCount = tasks.filter(\.isComplete).count
+        let completeStatus = completeCount == tasks.count
+        
         VStack {
+            // Top bar
             HStack {
                 BearImageCircle()
                 Text("Bear Minimum")
@@ -19,12 +27,53 @@ struct ContentView: View {
             }
             
             Text("Daily Tasks:").font(.title2)
-            TaskListView(tasks: self.$tasks)
+            Text(String(completeCount) + "/" + String(tasks.count))
+                .padding().font(.system(size: 20, weight: .bold, design: .default))
+            
+            // Task list
+            ScrollView {
+                ForEach($tasks) { $task in
+                    Toggle(task.id, isOn: $task.isComplete).padding().tint(.yellow)
+                        .onChange(of: task.isComplete) { value in
+                            TaskManager.save()
+                            
+                            if (task.isComplete) {
+                                successPlayer.stop()
+                                successPlayer.play()
+                            }
+                        }
+                    
+                }
+                .disabled(completeStatus)
+            }
+            
+            if  completeStatus {
+                Text("All done! Yippee!!!!")
+                Text("Come back tomorrow :)")
+                    .onAppear {
+                        if (!Calendar.current.isDateInToday(TaskManager.lastCompleted)) { // If a day has passed since last completed
+                            dayCount += 1
+                            TaskManager.lastCompleted = Date()
+                            successPlayer.stop()
+                            yipeePlayer.play()
+                        }
+                        
+                        TaskManager.save()
+                    }
+            }
+            
+            Text("Current Streak: " + String(dayCount) + " days.")
+                .padding()
+                .font(.system(size: 20, weight: .bold, design: .default))
             Spacer()
         }
-        .background(yellow ? .yellow.opacity(0.4) : .white)
-        .animation(.easeInOut, value: yellow)
-        
+        .background(completeStatus ? .yellow.opacity(0.4) : .white) // Fade to yellow when all tasks are done
+        .animation(.easeInOut, value: completeStatus)
+        .onAppear(){
+            // Initialize audio players
+            successPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource: "Success", ofType: "mp3")!))
+            yipeePlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource: "Yipee", ofType: "mp3")!))
+        }
     }
 }
 
